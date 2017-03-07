d04cdb11f0f30c60c3ab6223242d6da5c28e1827
eschofield <eschofield@CPM207.aht.org.uk>
2017-03-07 13:07:07 +0000

    updates to portal for crufts 2017 and ability to print out labels

--- .includes/js/functions.js
+++ .includes/js/functions.js
@@ -47,3 +47,80 @@ function showVetModal() {
 			}
 	});
 }
+
+function clearCanvas(){
+	var canvas = $('#canvasBarcode').get(0);
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function generateBarcode(value){
+	clearCanvas();
+	var settings = {
+	  	output: 'canvas',
+	    bgColor: '#FFFFFF',
+	    color: '#000000',
+		barWidth: 1,
+		barHeight: 70,
+		posX: 0,
+		posY: 0,
+		showHRI: 0
+	};
+	$("#canvasBarcode").show().barcode(value, "code39", settings);
+	
+	var canvas = document.getElementById("canvasBarcode");
+	return canvas.toDataURL("image/png");
+}
+
+
+function createAllLabels(swabs, testList, petName, ownerName, mainContact){
+	var doc = new jsPDF({
+		orientation: 'landscape',
+		unit: 'mm',
+		format: [100, 62]
+	});
+	
+	for (var i=0; i<testList.length; i++){
+		createLabel(doc, swabs[i], testList[i], petName, ownerName, mainContact);
+		if (i<testList.length-1)
+			doc.addPage();
+	}
+	
+	doc.autoPrint();
+	doc.output('dataurlnewwindow');
+}
+
+function createLabel(doc, swabID, testCode, dogName, ownerName, contact){
+	barcodeData = generateBarcode(swabID);
+	
+	doc.setFont("helvetica");
+	doc.setFontSize(18);
+	doc.setFontType("bold");
+	//doc.text(23, 12, 'Swab #'+swabID);
+	doc.text(25, 12, swabID);
+	
+	doc.setFontSize(18);
+	doc.setFontType("bold");
+	doc.text(3, 30, 'Test:');
+	doc.setFontSize(16);
+	doc.setFontType("normal");
+	doc.text(25, 30, testCode);
+	
+	doc.setFontSize(18);
+	doc.setFontType("bold");
+	doc.text(3, 40, 'Dog:');
+	doc.setFontSize(16);
+	doc.setFontType("normal");
+	doc.text(25, 40, dogName.toUpperCase());
+	
+	doc.setFontSize(18);
+	doc.setFontType("bold");
+	doc.text(3, 50, 'Contact:');
+	doc.setFontSize(16);
+	doc.setFontType("normal");
+	doc.text(35, 50, ownerName.toUpperCase());
+	doc.text(10, 58, contact);
+
+	doc.addImage(aht, 'PNG', 1, 1, 20, 18);
+	doc.addImage(barcodeData, 'PNG', 60, 1, 40, 40);
+}
